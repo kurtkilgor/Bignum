3e83e4d654476d33b222234d9afb626b252f2cba
Max Eskin <kurtkilgor@gmail.com>
2012-12-31 01:40:09 -0500

    Bignum struct, constructors and casts for int, uint, ulong, basic tests.

---
+++ Bignum/Bignum.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Bignum {
+    public struct Bignum {
+        // The bignum is composed of an int for the least significant bits
+        // and uints for the higher ones.
+        // The int carries the sign of the bignum.
+        readonly int tail;
+        readonly uint[] head;
+
+        public Bignum(int value) {
+            this.head = null;
+            this.tail = value;
+        }
+
+        public Bignum(uint value) {
+            uint top = (value & 0x80000000) == 0 ? 0 : (uint)1;
+            int bottom = (int)(value & 0x7FFFFFFF);
+
+            this.head = top > 0 ? new[] { top } : null;
+            this.tail = bottom;
+        }
+
+        public Bignum(ulong value) {
+            int bottom = (int)(value & 0x7FFFFFFF);
+            value = value >> 31;
+            uint chunk1 = (uint) (value & 0xFFFFFFFF);
+            uint chunk2 = (value & 0x100000000) == 0 ? 0 : (uint)1;
+
+            if (chunk1 == 0 && chunk2 == 0)
+                this.head = null;
+            else if (chunk2 == 0)
+                this.head = new[] { chunk1 };
+            else
+                this.head = new[] { chunk2, chunk1 };
+            this.tail = bottom;
+        }
+
+        public static explicit operator int(Bignum bignum) {
+            if (bignum.head != null)
+                throw new OverflowException(); // Value too big.
+
+            return bignum.tail;
+        }
+
+        public static explicit operator uint(Bignum bignum) {
+            if (bignum.tail < 0)
+                throw new OverflowException(); // Can't cast negative to a uint.
+
+            uint value = (uint)bignum.tail;
+            if (bignum.head != null) {
+                if (bignum.head.Length > 1 || bignum.head[0] > 1)
+                    throw new OverflowException(); // Value too big.
+
+                value += bignum.head[0] << 31;
+            }
+
+            return value;
+        }
+
+        public static explicit operator ulong(Bignum bignum) {
+            if (bignum.tail < 0)
+                throw new OverflowException(); // Can't cast negative to a ulong.
+
+            ulong value = (ulong)bignum.tail;
+            if (bignum.head != null) {
+                if(bignum.head.Length > 2)
+                    throw new OverflowException(); // Value too big.
+                else if (bignum.head.Length == 1)
+                    value += ((ulong) bignum.head[0]) << 31;
+                else if (bignum.head.Length == 2) {
+                    if (bignum.head[0] > 1)
+                        throw new OverflowException(); // Value too big.
+
+                    value += ((ulong) bignum.head[0]) << 63;
+                    value += ((ulong) bignum.head[1]) << 31;
+                }
+            }
+
+            return value;
+        }
+    }
+}
---
+++ BignumTest/BasicTest.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BignumTest {
+    using Bignum;
+
+    [TestClass]
+    public class BasicTest {
+        public static uint GetRandomUint() {
+            var random = new Random();
+            return (uint)(random.Next() + random.Next());
+        }
+
+        public static int GetRandomInt() {
+            var random = new Random();
+            var value = random.Next();
+            var sign = random.Next(0, 2) == 0 ? -1 : 1;
+            return value * sign;
+        }
+
+        public static ulong GetRandomUlong() {
+            var random = new Random();
+            return ((ulong)random.Next()) << 32 + random.Next();
+        }
+
+        public static long GetRandomLong() {
+            var random = new Random();
+            var value = ((long)random.Next()) << 32 + random.Next();
+            var sign = random.Next(0, 2) == 0 ? -1 : 1;
+            return value * sign;
+        }
+
+        [TestMethod]
+        public void CreateFromInt() {
+            var value = GetRandomInt();
+
+            Assert.AreEqual(value, (int)new Bignum(value));
+        }
+
+        [TestMethod]
+        public void CreateFromUint() {
+            var value = GetRandomUint();
+
+            Assert.AreEqual(value, (uint)new Bignum(value));
+        }
+
+        [TestMethod]
+        public void CreateFromUlong() {
+            var value = GetRandomUlong();
+
+            Assert.AreEqual(value, (ulong)new Bignum(value));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(OverflowException))]
+        public void CastToIntOverflow() {
+            var value = ((uint)Int32.MaxValue) + 1;
+            var bignum = new Bignum(value);
+
+            Assert.AreEqual(value, (uint)bignum);
+            var intValue = (int)bignum; // Should overflow
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(OverflowException))]
+        public void CastNegativeToUintOverflow() {
+            var value = -1;
+            var bignum = new Bignum(value);
+
+            Assert.AreEqual(value, (int)bignum);
+            var uintValue = (uint)bignum; // Should overflow
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(OverflowException))]
+        public void CastNegativeToUlongOverflow() {
+            var value = -1;
+            var bignum = new Bignum(value);
+
+            Assert.AreEqual(value, (int)bignum);
+            var uintValue = (ulong)bignum; // Should overflow
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(OverflowException))]
+        public void CastUlongToUintOverflow() {
+            var value = UInt64.MaxValue;
+            var bignum = new Bignum(value);
+
+            Assert.AreEqual(value, (ulong)bignum);
+            var uintValue = (uint)bignum; // Should overflow
+        }
+    }
+}
