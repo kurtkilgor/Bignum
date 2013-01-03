d44f5eae506656d45ea7c48ac641e90afb7c4836
Max Eskin <kurtkilgor@gmail.com>
2013-01-03 00:03:33 -0500

    Backtracking. Bignum renamed to Natural and only represents positive numbers.
    --HG--
    rename : Bignum/Bignum.cs => Bignum/Natural.cs
    rename : BignumTest/BasicTest.cs => BignumTest/BasicNaturalTest.cs

---
+++ Bignum/Natural.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Bignum {
+    public struct Natural {
+        readonly uint tail;
+        readonly uint[] head;
+
+        public Natural(uint value) {
+            this.head = null;
+            this.tail = value;
+        }       
+
+        public Natural(ulong value) {
+            this.tail = (uint)(value & 0xFFFFFFFF);
+            value = value >> 32;
+            uint chunk1 = (uint)(value & 0xFFFFFFFF);
+            
+            this.head = chunk1 == 0 ? null : new[] { chunk1 };            
+        }
+
+        #region Conversion Operators
+
+        public static implicit operator Natural(uint value) {
+            return new Natural(value);
+        }
+
+        public static implicit operator Natural(ulong value) {
+            return new Natural(value);
+        }
+
+        public static explicit operator int(Natural bignum) {
+            return (int)(uint)bignum;
+        }
+
+        public static explicit operator uint(Natural bignum) {
+            if (bignum.head != null) 
+                throw new OverflowException(); // Value too big.
+
+            return bignum.tail;
+        }
+
+        public static explicit operator long(Natural bignum) {
+            return (long)(ulong)bignum;
+        }
+
+        public static explicit operator ulong(Natural bignum) {
+            if (bignum.head == null)
+                return bignum.tail;
+            else if (bignum.head.Length == 1)
+                return (((ulong)bignum.head[0]) << 32) + bignum.tail;
+            else
+                throw new OverflowException();
+        }
+
+        #endregion
+
+        #region Equals and GetHashCode
+
+        public static bool operator ==(Natural a, Natural b) {
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(Natural a, Natural b) {
+            return !a.Equals(b);
+        }
+
+        public static bool Equals(Natural a, Natural b) {
+            if (a.tail != b.tail)
+                return false;
+
+            if (a.head == null ^ b.head == null)
+                return false;
+
+            if (a.head != null) {
+                if (a.head.Length != b.head.Length)
+                    return false;
+
+                for (int i = 0; i < a.head.Length; i++) {
+                    if (a.head[i] != b.head[i])
+                        return false;
+                }
+            }
+
+            return true;
+        }
+
+        public override bool Equals(object obj) {
+            if (!(obj is Natural))
+                return false;
+
+            var bignum = (Natural)obj;
+            return Equals(this, bignum);
+        }
+
+        public override int GetHashCode() {
+            // Based on http://stackoverflow.com/a/263416
+            uint value = 17;
+            value = value * 23 + (tail);
+            if (head != null)
+                foreach (var item in head) {
+                    value = value * 23 + (item & 0x7FFFFFFF);
+                }
+
+            return (int) value;
+        }
+
+        #endregion
+    }
+}
---
+++ BignumTest/BasicNaturalTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BignumTest {
+    using Bignum;
+
+    [TestClass]
+    public class BasicNaturalTest {
+        public static uint GetRandomUint() {
+            var random = new Random();
+            return (uint)(random.Next() + random.Next());
+        }
+
+        public static int GetRandomInt() {
+            var random = new Random();
+            var value = random.Next();
+            var sign = random.Next(0, 2) == 0 ? -1 : 1;
+            return value * sign;
+        }
+
+        public static ulong GetRandomUlong() {
+            var random = new Random();
+            return ((ulong)random.Next()) << 32 + random.Next();
+        }
+
+        public static long GetRandomLong() {
+            var random = new Random();
+            var value = ((long)random.Next()) << 32 + random.Next();
+            var sign = random.Next(0, 2) == 0 ? -1 : 1;
+            return value * sign;
+        }
+
+        [TestMethod]
+        public void CreateFromUint() {
+            var value = GetRandomUint();
+
+            Assert.AreEqual(value, (uint)new Natural(value));
+            Assert.AreEqual(UInt32.MinValue, (uint)new Natural(UInt32.MinValue));
+            Assert.AreEqual(UInt32.MaxValue, (uint)new Natural(UInt32.MaxValue));
+
+        }
+
+        [TestMethod]
+        public void CreateFromUlong() {
+            var value = GetRandomUlong();
+
+            Assert.AreEqual(value, (ulong)new Natural(value));
+            Assert.AreEqual(UInt64.MinValue, (ulong)new Natural(UInt64.MinValue));
+            Assert.AreEqual(UInt64.MaxValue, (ulong)new Natural(UInt64.MaxValue));
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(OverflowException))]
+        public void CastUlongToUintOverflow() {
+            var value = UInt64.MaxValue;
+            var bignum = new Natural(value);
+
+            Assert.AreEqual(value, (ulong)bignum);
+            var uintValue = (uint)bignum; // Should overflow
+        }
+
+        [TestMethod]
+        public void BasicEquality() {
+            var value = GetRandomUlong();
+
+            var v1 = new Natural(value);
+            var v2 = new Natural(value);
+
+            Assert.IsTrue(v1.Equals(v2));
+            Assert.IsTrue(v2.Equals(v1));
+            Assert.IsTrue(v1 == v2);
+            Assert.IsTrue(v1 == value);
+            Assert.IsTrue(v2 == value);
+        }
+    }
+}
